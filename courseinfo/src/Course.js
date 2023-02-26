4f9df3d9523e64ddfe719cdc8d9398ebfd3b6b7e
EdgarGrcs <edgar.gricius2@gmail.com>
2023-02-27 00:00:11 +0100

    initial commit

--- courseinfo/src/Course.js
+++ courseinfo/src/Course.js
@@ -5,12 +5,21 @@ const Course = (props) => {
 
     const {course} = props;
 
-    
- 
+
     return (
         <div>
-         
-           
+         {course.map((names) => (
+            <>
+           <Header key={names.id} name={names.name}/>
+           {names.parts.map((part) => (
+            <>
+            <Part key={part.id} name={part.name} exercises={part.exercises}/>
+            </>
+           )
+           )}
+           <h4>total of {names.parts.reduce((sum,part) => (sum += part.exercises),0)} exercises </h4>
+           </>
+         ))}
         </div>
     )
 }
