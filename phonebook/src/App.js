440745ab6d348eca9a532241d791177d4bd1144a
EdgarGrcs <edgar.gricius2@gmail.com>
2023-06-09 12:40:00 +0200

    refactor lines

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -5,6 +5,7 @@ import Persons from './Components/Persons'
 import personBackend from './services/ListBackend'
 import Notification from './Components/Notification'
 import Error from './Components/Error'
+import { logDOM } from '@testing-library/react'
 
 
 const App = () => {
@@ -15,10 +16,7 @@ const App = () => {
   const [message, setMessage] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
 
-
- // 2.17 left to do
-
-
+ 
   useEffect(() => {
     personBackend
     .getAll()
@@ -38,26 +36,20 @@ const App = () => {
 
   const handleSearchName = (event) => {
     setSearchName(event.target.value);
-
     const list = persons.filter((names) => names.name.toLowerCase().includes((event.target.value).toLowerCase()))
-  
     setPersons(list);
   }
 
   const addPerson = (event) => {
     event.preventDefault();
-
     if (persons.some(person => person.name.toLowerCase() === newName) && persons.some(person => person.number === newPhone)){
        return alert(`${newName} is already added to phonebook`)
     } 
-
     if (persons.some(person => person.name.toLowerCase() === newName.toLowerCase()) && persons.some(person => person.number !== newPhone)){
     
       if( window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)){
-
         const person = persons.filter(person => newName.toLowerCase() === person.name.toLowerCase());
         person[0].number = newPhone;
-      
         personBackend
         .update(person[0].id,person[0])
         .then(response => {
@@ -75,46 +67,39 @@ const App = () => {
         .create(personObject)
         .then(response => {
           setPersons(persons.concat(response.data))
+          setMessage(`Added ${newName}`);
+            setTimeout(() => {
+              setMessage(null)
+            },5000);
+          setNewName("");
+          setNewPhone("");
+        }).catch(error => {
+          setErrorMessage(error.message );
+          setTimeout(() => {
+            setErrorMessage(null)
+          },5000);
+          setNewName("");
+          setNewPhone("");
         })
-
-        setMessage(`Added ${newName}`);
-
-        setTimeout(() => {
-          setMessage(null)
-        },5000);
-
-        
-      setNewName("");
-      setNewPhone("");    
-
+        // error.response.data.ValidationError doesnt work in accessing the error message from mongoose validator
   }
 
   const handleDelete = (id) => {
-
    let personList = persons.filter(person => id !== person.id); // all which are not
    let personList2 = persons.filter(person => id === person.id); //single object
 
-   
-
    if (window.confirm(`Delete ${personList2[0].name} ?`)){
-
    personBackend
     .remove(id,personList2)
     .then(response => {
       setPersons(personList);
     }).catch((error) => {
       setErrorMessage(`Information of ${personList2[0].name} has already been removed from server`);
-
       setTimeout(() => {
         setErrorMessage(null)
       },5000);
-    
     setPersons(persons.filter(n => n.id !== id));
-    
     })
-
-    
-
   }
   }
 
